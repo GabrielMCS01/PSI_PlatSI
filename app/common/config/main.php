ffd36c2440ff29a2d36389f1e88d6302e7e3063e
Gabriel Silva <gabrielcamposilva@sapo.pt>
2021-11-02 13:09:09 +0000

    Configuração do DbManager (fazer migrations)

--- app/common/config/main.php
+++ app/common/config/main.php
@@ -9,5 +9,11 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            // uncomment if you want to cache RBAC items hierarchy
+            // 'cache' => 'cache',
+        ],
+
     ],
 ];
