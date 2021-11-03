05b92450358cc73277c522601158781e0dcec958
iuricarras <iuri.carrasqueiro@gmail.com>
2021-11-03 14:53:57 +0000

    Acesso ao Backend/Frontend bloqueado a utilizadores inválidos

--- app/frontend/controllers/SiteController.php
+++ app/frontend/controllers/SiteController.php
@@ -15,6 +15,7 @@ use frontend\models\PasswordResetRequestForm;
 use frontend\models\ResetPasswordForm;
 use frontend\models\SignupForm;
 use frontend\models\ContactForm;
+use yii\web\User;
 
 /**
  * Site controller
@@ -85,13 +86,24 @@ class SiteController extends Controller
      */
     public function actionLogin()
     {
+        $auth = Yii::$app->authManager;
+
         if (!Yii::$app->user->isGuest) {
+
+
             return $this->goHome();
         }
 
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
+            if($auth->checkAccess(Yii::$app->user->getId(), "frontendAccess")){
+                return $this->goBack();
+            }else{
+
+                Yii::$app->user->logout();
+
+                return $this->goHome();
+            }
         }
 
         $model->password = '';
--- app/frontend/models/SignupForm.php
+++ app/frontend/models/SignupForm.php
@@ -45,18 +45,21 @@ class SignupForm extends Model
      */
     public function signup()
     {
-        if (!$this->validate()) {
-            return null;
-        }
-        
-        $user = new User();
-        $user->username = $this->username;
-        $user->email = $this->email;
-        $user->setPassword($this->password);
-        $user->generateAuthKey();
-        $user->generateEmailVerificationToken();
+        if ($this->validate()) {
+            $user = new User();
+            $user->username = $this->username;
+            $user->email = $this->email;
+            $user->setPassword($this->password);
+            $user->generateAuthKey();
+            $user->save(false);
+            $auth = Yii::$app->authManager;
+            $userRole = $auth->getRole('user');
+            $auth->assign($userRole, $user->getId());
 
-        return $user->save() && $this->sendEmail($user);
+            return $user;
+        }
+
+        return null;
     }
 
     /**
