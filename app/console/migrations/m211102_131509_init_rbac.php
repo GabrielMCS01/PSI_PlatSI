e289a76dc4c13e9e429513e580955e55031674bd
Gabriel Silva <gabrielcamposilva@sapo.pt>
2021-11-03 10:35:59 +0000

    Roles Admin e User e criadas as devidas permissoes (Correr Migrations)

--- app/console/migrations/m211102_131509_init_rbac.php
+++ app/console/migrations/m211102_131509_init_rbac.php
@@ -12,7 +12,73 @@ class m211102_131509_init_rbac extends Migration
      */
     public function safeUp()
     {
+        $auth = Yii::$app->authManager;
 
+        // ------------------------------ USER ---------------------------------------------
+        // add "frontendAccess" permission
+        $frontendAccess = $auth->createPermission('frontendAccess');
+        $frontendAccess->description = 'Frontend Access';
+        $auth->add($frontendAccess);
+
+        // add "createActivity" permission
+        $createActivity = $auth->createPermission('createActivity');
+        $createActivity->description = 'Create a Activity';
+        $auth->add($createActivity);
+
+        // add "updateActivity" permission
+        $updateActivity = $auth->createPermission('updateActivity');
+        $updateActivity->description = 'Update Activity (User logged)';
+        $auth->add($updateActivity);
+
+        // add "deleteActivity" permission
+        $deleteActivity = $auth->createPermission('deleteActivity');
+        $deleteActivity->description = 'Delete Activity (User logged)';
+        $auth->add($deleteActivity);
+
+        // add "viewActivity" permission
+        $viewActivity = $auth->createPermission('viewActivity');
+        $viewActivity->description = 'View Activity (User logged)';
+        $auth->add($viewActivity);
+
+        // add "updateProfile" permission
+        $updateProfile = $auth->createPermission('updateProfile');
+        $updateProfile->description = 'Update Profile (User logged)';
+        $auth->add($updateProfile);
+
+        // add "deleteProfile" permission
+        $deleteProfile = $auth->createPermission('deleteProfile');
+        $deleteProfile->description = 'Delete Profile (User logged)';
+        $auth->add($deleteProfile);
+
+        // add "viewProfile" permission
+        $viewProfile = $auth->createPermission('viewProfile');
+        $viewProfile->description = 'View Profile (User logged)';
+        $auth->add($viewProfile);
+
+        // add "author" role and give him profile and activity roles
+        $user = $auth->createRole('user');
+        $auth->add($user);
+        $auth->addChild($user, $frontendAccess);
+        $auth->addChild($user, $createActivity);
+        $auth->addChild($user, $updateActivity);
+        $auth->addChild($user, $deleteActivity);
+        $auth->addChild($user, $viewActivity);
+        $auth->addChild($user, $updateProfile);
+        $auth->addChild($user, $deleteProfile);
+        $auth->addChild($user, $viewProfile);
+        // ---------------------------- USER --------------------------------------
+
+        // ---------------------------- ADMIN -------------------------------------
+        // add "backendAccess" permission
+        $backendAccess = $auth->createPermission('backendAccess');
+        $backendAccess->description = 'Backend Access';
+        $auth->add($backendAccess);
+
+        // add "admin" role
+        $admin = $auth->createRole('admin');
+        $auth->add($admin);
+        $auth->addChild($admin, $backendAccess);
+        // --------------------------- ADMIN ---------------------------------------
     }
 
     /**
@@ -20,23 +86,8 @@ class m211102_131509_init_rbac extends Migration
      */
     public function safeDown()
     {
-        echo "m211102_131509_init_rbac cannot be reverted.\n";
+        $auth = Yii::$app->authManager;
 
-        return false;
+        $auth->removeAll();
     }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m211102_131509_init_rbac cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
