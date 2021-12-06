c87305385eb70cc2057e0736cb7e66f0746517b9
Gabriel Silva <gabrielcamposilva@sapo.pt>
2021-12-06 16:21:39 +0000

    quando inicia sessão envia variavel com sucess

--- app/backend/modules/v1/controllers/LoginController.php
+++ app/backend/modules/v1/controllers/LoginController.php
@@ -47,8 +47,12 @@ class LoginController extends ActiveController
                 $user->auth_key = Yii::$app->security->generateRandomString();
                 $user->save();
 
+                $json = "{ \"sucess\": true,
+                        \"token\": \"$user->auth_key\"
+                    
+                }";
                 // Retorna o token
-                return $user->auth_key;
+                return $json;
             }else{
                 $message = "Utilizador sem acesso á aplicação";
             }
