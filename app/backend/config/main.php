144e297c84a0dfb02958c368f7adf8bcb024c24b
iuricarras <iuri.carrasqueiro@gmail.com>
2021-12-06 13:08:31 +0000

    alteração dos ficheiros para o servidor

--- app/backend/config/main.php
+++ app/backend/config/main.php
@@ -23,16 +23,27 @@ return [
                 'application/json' => 'yii\web\JsonParser',
             ],
             'csrfParam' => '_csrf-backend',
+            'csrfCookie' => [
+            'httpOnly' => true,
+            'path' => '/admin',
+            ],
         ],
         'user' => [
-            'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+             'identityClass' => 'common\models\User',
+        'enableAutoLogin' => true,
+        'identityCookie' => [
+            'name' => '_identity-backend',
+            'path' => '/admin',
+            'httpOnly' => true,
+        ],
         ],
         'session' => [
-            // this is the name of the session cookie used for login on the backend
-            'name' => 'advanced-backend',
+        // this is the name of the session cookie used for login on the backend
+        'name' => 'advanced-backend',
+        'cookieParams' => [
+            'path' => '/admin',
         ],
+    ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
