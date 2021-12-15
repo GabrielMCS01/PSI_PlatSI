2bd82630180d04cb6ce0505575c8c1563e39b9b9
Gabriel Silva <gabrielcamposilva@sapo.pt>
2021-12-15 15:35:54 +0000

    mini correção

--- app/backend/views/user/index.php
+++ app/backend/views/user/index.php
@@ -29,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
                             'email:email',
                             [
                                 'label' => 'Tipo de Utilizador',
-                                'value' => $dataProvider->authassignment->item_name,
+                                'value' => $searchModel->authassignment->item_name,
                             ],
                             //'auth_key',
                             //'password_hash',
