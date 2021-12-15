5ec1b778b4548e27b410e223fd423b11c0da1758
Gabriel Silva <gabrielcamposilva@sapo.pt>
2021-12-15 14:15:15 +0000

    mais tentativas de correção

--- app/backend/controllers/UserController.php
+++ app/backend/controllers/UserController.php
@@ -100,13 +100,13 @@ class UserController extends Controller
 
         // Receber cada post individualmente (variaveis e atribuir aos devidos models)
         if (Yii::$app->request->isPost) {
-            var_dump(Yii::$app->request->Post("item_name"));
+            var_dump(Yii::$app->request->Post('item_name'));
 
             $user_info->primeiro_nome = Yii::$app->request->post("primeiro_nome");
             $user_info->ultimo_nome = Yii::$app->request->post("ultimo_nome");
             $user_info->data_nascimento = Yii::$app->request->post("data_nascimento");
 
-            $auth_model->item_name = Yii::$app->request->post("item_name");
+            $auth_model->item_name = Yii::$app->request->post('item_name');
 
             if($model->save() && $auth_model->save() && $user_info->save()){
                 return $this->redirect(['view', 'id' => $model->id]);
