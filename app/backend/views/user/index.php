dc0789246f68036824aae49fe499b5ed5a74f2e7
Gabriel Silva <gabrielcamposilva@sapo.pt>
2021-12-15 15:40:22 +0000

    dump

--- app/backend/views/user/index.php
+++ app/backend/views/user/index.php
@@ -27,10 +27,10 @@ $this->params['breadcrumbs'][] = $this->title;
                             'id',
                             'username',
                             'email:email',
-                            [
+                            /*[
                                 'label' => 'Tipo de Utilizador',
                                 'value' => $searchModel->authassignment->item_name,
-                            ],
+                            ],*/
                             //'auth_key',
                             //'password_hash',
                             //'password_reset_token',
@@ -45,7 +45,8 @@ $this->params['breadcrumbs'][] = $this->title;
                         'pager' => [
                             'class' => 'yii\bootstrap4\LinkPager',
                         ]
-                    ]); ?>
+                    ]);
+                    var_dump($dataProvider);?>
 
 
                 </div>
