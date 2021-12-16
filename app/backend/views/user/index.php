652f685a41deebe49f4a6bf25c11912d484b490e
Gabriel Silva <gabrielcamposilva@sapo.pt>
2021-12-16 10:56:51 +0000

    aaa

--- app/backend/views/user/index.php
+++ app/backend/views/user/index.php
@@ -62,7 +62,7 @@ for ($i = 0; $i < count($roles); $i++){
                         ]
                     ]);?>
                     <br>
-                    <?$i++;
+                    <?php $i++;
                     }?>
                 </div>
                 <!--.card-body-->
