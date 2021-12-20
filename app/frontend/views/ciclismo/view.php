29e106c0379a5c547a51f45ac6d5e4070af625c3
iuricarras <iuri.carrasqueiro@gmail.com>
2021-12-20 12:30:52 +0000

    Utilizar rota da db

--- app/frontend/views/ciclismo/view.php
+++ app/frontend/views/ciclismo/view.php
@@ -43,6 +43,8 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
 </div>
 <script>
+
+    var polyline = require('@mapbox/polyline');
     mapboxgl.accessToken = 'pk.eyJ1IjoiaXVyaWNhcnJhcyIsImEiOiJja3V3aDJrZWEwNjhuMm5xd3hqNHRuODdiIn0.Yztl8wZEMrxIlkEVwt1zgw';
     const map = new mapboxgl.Map({
         container: 'map',
@@ -52,6 +54,7 @@ $this->params['breadcrumbs'][] = $this->title;
     });
 
     map.on('load', () => {
+        var array = polyline.decode(<?= $model->rota?>, 6);
         map.addSource('route', {
             'type': 'geojson',
             'data': {
@@ -59,29 +62,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'properties': {},
                 'geometry': {
                     'type': 'LineString',
-                    'coordinates': [
-                        [-122.483696, 37.833818],
-                        [-122.483482, 37.833174],
-                        [-122.483396, 37.8327],
-                        [-122.483568, 37.832056],
-                        [-122.48404, 37.831141],
-                        [-122.48404, 37.830497],
-                        [-122.483482, 37.82992],
-                        [-122.483568, 37.829548],
-                        [-122.48507, 37.829446],
-                        [-122.4861, 37.828802],
-                        [-122.486958, 37.82931],
-                        [-122.487001, 37.830802],
-                        [-122.487516, 37.831683],
-                        [-122.488031, 37.832158],
-                        [-122.488889, 37.832971],
-                        [-122.489876, 37.832632],
-                        [-122.490434, 37.832937],
-                        [-122.49125, 37.832429],
-                        [-122.491636, 37.832564],
-                        [-122.492237, 37.833378],
-                        [-122.493782, 37.833683]
-                    ]
+                    'coordinates': array
                 }
             }
         });
--- app/frontend/views/publicacao/index.php
+++ app/frontend/views/publicacao/index.php
@@ -76,6 +76,7 @@ $this->registerJs("
                 var divElts = document.getElementById("map");
                 divElts.setAttribute('id', "map" + <?=$publicacao->id?>);
 
+                var polyline = require('@mapbox/polyline');
 
                 mapboxgl.accessToken = 'pk.eyJ1IjoiaXVyaWNhcnJhcyIsImEiOiJja3V3aDJrZWEwNjhuMm5xd3hqNHRuODdiIn0.Yztl8wZEMrxIlkEVwt1zgw';
                 map[<?= $publicacao->id?>] = new mapboxgl.Map({
@@ -84,6 +85,8 @@ $this->registerJs("
                     center: [-122.486052, 37.830348],
                     zoom: 14
                 }).on('load', () => {
+
+                    var array = polyline.decode(<?= $publicacao->ciclismo->rota?>, 6);
                     map[<?= $publicacao->id?>].addSource('id' + j, {
                         'type': 'geojson',
                         'data': {
@@ -91,29 +94,7 @@ $this->registerJs("
                             'properties': {},
                             'geometry': {
                                 'type': 'LineString',
-                                'coordinates': [
-                                    [-122.483696, 37.833818],
-                                    [-122.483482, 37.833174],
-                                    [-122.483396, 37.8327],
-                                    [-122.483568, 37.832056],
-                                    [-122.48404, 37.831141],
-                                    [-122.48404, 37.830497],
-                                    [-122.483482, 37.82992],
-                                    [-122.483568, 37.829548],
-                                    [-122.48507, 37.829446],
-                                    [-122.4861, 37.828802],
-                                    [-122.486958, 37.82931],
-                                    [-122.487001, 37.830802],
-                                    [-122.487516, 37.831683],
-                                    [-122.488031, 37.832158],
-                                    [-122.488889, 37.832971],
-                                    [-122.489876, 37.832632],
-                                    [-122.490434, 37.832937],
-                                    [-122.49125, 37.832429],
-                                    [-122.491636, 37.832564],
-                                    [-122.492237, 37.833378],
-                                    [-122.493782, 37.833683]
-                                ]
+                                'coordinates': array
                             }
                         }
                     });
