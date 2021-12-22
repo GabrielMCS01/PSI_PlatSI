91dcf0d4b8f96d668212ad6a44e86890d03313e3
iuricarras <iuri.carrasqueiro@gmail.com>
2021-12-22 11:25:59 +0000

    Criação da template do formulario

--- app/frontend/views/comentario/_post.php
+++ app/frontend/views/comentario/_post.php
@@ -1,9 +1,12 @@
 <?php
+
 use yii\helpers\Html;
 use yii\helpers\HtmlPurifier;
-?>
-    <div class="post">
-    <h2><?= Html::encode($model->createtime) ?></h2>
 
-<?= HtmlPurifier::process($model->content) ?>
-    </div><?php
+?>
+<div class="post">
+    <br>
+    <h3><strong><?= Html::encode($model->user->username) ?></strong><?= Html::encode($model->createtime) ?></h3>
+    <p><?= HtmlPurifier::process($model->content) ?></p>
+    <br>
+</div>
