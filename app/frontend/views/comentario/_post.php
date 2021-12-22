75ae52b152806773082befacdf3cdf2ab9e567dc
iuricarras <iuri.carrasqueiro@gmail.com>
2021-12-22 12:12:09 +0000

    ALteração da template dos comentarios

--- app/frontend/views/comentario/_post.php
+++ app/frontend/views/comentario/_post.php
@@ -5,9 +5,10 @@ use yii\helpers\HtmlPurifier;
 
 ?>
 <div class="post">
-    <br>
     <h3><strong><?= Html::encode($model->user->username) ?></strong></h3>
     <h5><?= HtmlPurifier::process($model->content) ?></h5>
     <?= Html::encode($model->createtime) ?>
+    <br>
+    <br>
     <br>
 </div>
