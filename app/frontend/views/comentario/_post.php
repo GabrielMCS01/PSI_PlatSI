41a27d418a9a579885c3b4df844ba569492d9c7d
iuricarras <iuri.carrasqueiro@gmail.com>
2021-12-22 11:40:26 +0000

    Continuação

--- app/frontend/views/comentario/_post.php
+++ app/frontend/views/comentario/_post.php
@@ -6,7 +6,8 @@ use yii\helpers\HtmlPurifier;
 ?>
 <div class="post">
     <br>
-    <h3><strong><?= Html::encode($model->user->username) ?></strong></h3> - <?= Html::encode($model->createtime) ?>
+    <h3><strong><?= Html::encode($model->user->username) ?></strong></h3>
+    <?= Html::encode($model->createtime) ?>
     <p><?= HtmlPurifier::process($model->content) ?></p>
     <br>
 </div>
