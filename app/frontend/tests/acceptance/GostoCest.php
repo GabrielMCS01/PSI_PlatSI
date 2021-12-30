6c98fd4a71e2e75480aaccbb91f403df5f32c1b6
Gabriel Silva <gabrielcamposilva@sapo.pt>
2021-12-30 13:52:52 +0000

    teste de aceitação para login e editar perfil

--- app/frontend/tests/acceptance/GostoCest.php
+++ app/frontend/tests/acceptance/GostoCest.php
@@ -7,8 +7,23 @@ class GostoCest
     {
     }
 
-    // tests
-    public function tryToTest(AcceptanceTester $I)
+    public function CriarEliminarGostoTest(AcceptanceTester $I)
     {
+        $I->amOnPage(\Yii::$app->homeUrl);
+
+        $I->wait(2);
+        // Página de publicações
+        $I->amOnRoute('publicacao/index'); // Página de Feed de noticias
+        $I->see('Publicações', 'h1');
+        $I->see('Percurso de teste', 'h3');
+        $I->seeLink('');
+        $I->click('', '#2');
+
+        $I->wait(2);
+        // Verifica se o comentário foi criado
+        $I->SeeRecord('common\models\Gosto', [
+            'user_id' => 5,
+            'publicacao_id' => 1
+        ]);
     }
 }
